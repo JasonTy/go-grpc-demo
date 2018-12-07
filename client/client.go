1f8ff4cf46eb3841e046fbf8987092a6a8289abd
JasonTy <1026443062@qq.com>
2018-12-07 10:58:23 +0800

    update

---
+++ client/client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	pb "go/grpc/demo/protocol"
+)
+
+
+
+const (
+	address     = "localhost:5001"
+	defaultName = "world"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	r, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module go/grpc/demo
+
+require (
+	github.com/golang/protobuf v1.2.0
+	golang.org/x/net v0.0.0-20181201002055-351d144fa1fc
+	golang.org/x/sync v0.0.0-20181108010431-42b317875d0f // indirect
+	google.golang.org/grpc v1.17.0
+)
